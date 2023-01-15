3178433791f97c7c9f037576fbd9b1297734f283
Ng Chin Chia <billyngchinchia@gmail.com>
2023-01-15 17:37:17 +0800

    useEffectHook & Axios

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,24 @@
-
-
+import Axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [catFact, setCatFact] = useState("");
+
+  const fetchCatFact = () => {
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+      setCatFact(res.data.fact);
+    });
+  };
+
+  useEffect(() => {
+   fetchCatFact();
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>Cat Fact</h1>
+      <button onClick={fetchCatFact}> Generate a New Cat Fact </button>
+      <p>{catFact}</p>
     </div>
   );
 }
